1eade15cd67562d68fc7d44e58559aa160abc693
shahabsaqib220 <shahabsaqib220@gmail.com>
2024-10-10 00:17:30 +0500

    Nav has been setup

--- routers/UserViewAdsRouter.js
+++ routers/UserViewAdsRouter.js
@@ -6,7 +6,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 // Protected route to get all ads posted by the authenticated user
 // In your ads route (backend)
 router.get('/myads', authMiddleware, async (req, res) => {
-  const { page = 1, limit = 5 } = req.query; // Default values for page and limit
+  const { page = 1, limit = 8 } = req.query; // Default values for page and limit
 
   try {
     const adsCount = await Ad.countDocuments({ userId: req.userId, adStatus: "available" });
