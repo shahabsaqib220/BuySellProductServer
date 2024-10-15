b2ed0d290e090653b71eca007d0b56e5cf729575
shahabsaqib220 <shahabsaqib220@gmail.com>
2024-10-15 14:41:09 +0500

    Cores updated

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ const app = express();
 
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -36,20 +38,20 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 
-app.use(process.env.API_V1_OAUTH, authRoutes);
+// app.use(process.env.API_V1_OAUTH, authRoutes);
 app.use("/api/profile-image",  profileImage);
-app.use(process.env.API_V3_OAUTH, userAds);
+// app.use(process.env.API_V3_OAUTH, userAds);
 app.use("/api/userlogin",userLogin);
-app.use(process.env.API_V5_OAUTH, productList);
-app.use(process.env.API_V6_OAUTH, myAdsRouter);
-app.use(process.env.API_V7_OAUTH, adDelete);
-app.use(process.env.API_V8_OAUTH,ProductDetailsRouter );
-app.use(process.env.API_V9_OAUTH,OtherRelatedProductRouter );
-app.use(process.env.API_V10_OAUTH, userProfileImage );
-app.use(process.env.API_V11_OAUTH, soldOutRouter );
-app.use(process.env.API_V12_OAUTH, userCartItem );
-app.use(process.env.API_V13_OAUTH, cartItemRouter );
-app.use(process.env.API_V14_OAUTH, PasswordChangeRouter);
+// app.use(process.env.API_V5_OAUTH, productList);
+// app.use(process.env.API_V6_OAUTH, myAdsRouter);
+// app.use(process.env.API_V7_OAUTH, adDelete);
+// app.use(process.env.API_V8_OAUTH,ProductDetailsRouter );
+// app.use(process.env.API_V9_OAUTH,OtherRelatedProductRouter );
+// app.use(process.env.API_V10_OAUTH, userProfileImage );
+// app.use(process.env.API_V11_OAUTH, soldOutRouter );
+// app.use(process.env.API_V12_OAUTH, userCartItem );
+// app.use(process.env.API_V13_OAUTH, cartItemRouter );
+// app.use(process.env.API_V14_OAUTH, PasswordChangeRouter);
 
 
 
