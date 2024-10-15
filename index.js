ad57257d76c680391404a959bb038b7043ab8213
shahabsaqib220 <shahabsaqib220@gmail.com>
2024-10-15 23:33:54 +0500

    API removed

--- index.js
+++ index.js
@@ -3,23 +3,11 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const errorHandler = require('./middleware/errorHandler');
 require('express-async-errors');
-const authRoutes = require('./routers/registrationUserRouter');
-const loginUser = require("./routers/UserLoginRouter")
-const profileImage = require("./routers/ProfileImageUserRouter");
-const authMiddleware = require('./middleware/authMiddleware')
-const userAds = require('./routers/UsersAdsRouter')
+
 const userLogin= require('./routers/UserLoginRouter')
-const adDelete = require("./routers/DeleteUserAdRouter")
-const productList = require('./routers/ProductListRouter')
-const OtherRelatedProductRouter = require("./routers/OtherRelatedProductRouter")
-const myAdsRouter = require('./routers/UserViewAdsRouter')
-const ProductDetailsRouter = require('./routers/ProductDetailsRouter')
-const userProfileImage = require('./routers/userProfileImageRouter')
+
 const bodyParser = require('body-parser');
-const soldOutRouter = require("./routers/UserSoldOutProductRouter")
-const userCartItem = require("./routers/UserCartItemRouter")
-const cartItemRouter = require("./routers/CartItemNavigatiorRouter")
-const PasswordChangeRouter = require("./routers/PasswordChangeRouter")
+
 const app = express();
 
 require('dotenv').config();
@@ -37,20 +25,9 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use(process.env.API_V1_OAUTH, authRoutes);
-app.use(process.env.API_V2_OAUTH,  profileImage);
-app.use(process.env.API_V3_OAUTH, userAds);
+
 app.use(process.env.API_V4_OAUTH,userLogin);
-app.use(process.env.API_V5_OAUTH, productList);
-app.use(process.env.API_V6_OAUTH, myAdsRouter);
-app.use(process.env.API_V7_OAUTH, adDelete);
-app.use(process.env.API_V8_OAUTH,ProductDetailsRouter );
-app.use(process.env.API_V9_OAUTH,OtherRelatedProductRouter );
-app.use(process.env.API_V10_OAUTH, userProfileImage );
-app.use(process.env.API_V11_OAUTH, soldOutRouter );
-app.use(process.env.API_V12_OAUTH, userCartItem );
-app.use(process.env.API_V13_OAUTH, cartItemRouter );
-app.use(process.env.API_V14_OAUTH, PasswordChangeRouter);
+
 
 
 
@@ -71,7 +48,7 @@ app.get('/', (req, res) => {
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://shahabsaqib220:Pakistan1122@cluster0.ugd18.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
+        await mongoose.connect(process.env.MONGO_DB_URL,{
            
         })
         console.log("MongoDb Connected");
