ab963a9bc5e39cdd59c0362f7c097bf00edaf629
shahabsaqib220 <shahabsaqib220@gmail.com>
2024-10-16 14:39:53 +0500

    Cors policy

--- index.js
+++ index.js
@@ -33,11 +33,7 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: 'http://localhost:3000', // Allow only your frontend origin
-    methods: 'GET,POST,PUT,DELETE', // Specify allowed methods if necessary
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 
