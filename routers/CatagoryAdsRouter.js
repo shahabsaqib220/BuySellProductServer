74d4ce86dbf7da140351c0da6ba51d1c5003ead3
shahabsaqib220 <shahabsaqib220@gmail.com>
2024-10-24 01:29:20 +0500

    Cart router are setup

--- routers/CatagoryAdsRouter.js
+++ routers/CatagoryAdsRouter.js
@@ -1,19 +1,21 @@
-
 const express = require('express');
 const router = express.Router();
 const Ad = require('../models/UsersAdsModel'); // Assuming Ad is your mongoose model
 
-// Fetch ads by category
+// Fetch ads by category and with adStatus "available"
 router.get('/:category', async (req, res) => {
   const { category } = req.params;
 
   try {
-    // Use a case-insensitive search to find ads by category
-    const ads = await Ad.find({ category: { $regex: new RegExp(category, "i") } });
-    
+    // Use a case-insensitive search to find ads by category and check adStatus
+    const ads = await Ad.find({ 
+      category: { $regex: new RegExp(category, "i") },
+      adStatus: 'available' // Filter by adStatus
+    });
+
     // Check if ads exist for the category
     if (!ads.length) {
-      return res.status(404).json({ message: `No ads found for category: ${category}` });
+      return res.status(404).json({ message: `No available ads found for category: ${category}` });
     }
 
     // Return ads to the frontend
